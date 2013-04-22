e39bf6e18064de0211b1a886834c36c3fba7d2a7
hornew <hornew@goldmail.etsu.edu>
2013-04-22 13:40:48 -0400

    Matrix

---
+++ Horne_Matrix_Lab/Horne_Matrix_Lab/Matrix.cs
@@ -0,0 +1,81 @@
+﻿/*************************************************************************
+ * File Name         :  Matrix.cs
+ * Purpose            : Represents a matrix. Allows adding of 2 matrices
+ *                          with + operator, setting of values in a specific 
+ *                          matrix location, and printing of matrix values.
+ *                          
+ * Author               : Austin Horne    E-mail: hornew@goldmail.etsu.edu       
+ * Course               : CSCI 3230 - Algorithms
+ * Create Date       : February 11, 2013
+ * 
+ * Modified Date    : February 11, 2013
+ * Modified by       : Austin Horne
+**************************************************************************
+*/
+using System;
+
+namespace Horne_Matrix_Lab
+{
+    class Matrix
+    {
+        private int nRows;               //stores the number of rows for the matrix
+        private int nCols;               //stores the number of columns for the matrix
+        private double[,] m;             //2D array that stores the matrix values
+
+        public Matrix(int rows, int col)
+        {
+            this.nCols = col;
+            this.nRows = rows;
+            m = new double[nRows, nCols];
+        }//2 param constructor
+
+        //Place a value in the 2D array at row, col
+        public void SetValue(int row, int col, double value)
+        {
+            m[row, col] = value;
+        }//SetValue(int, int, double)
+
+        //Return the 2D array row by row as a string.
+        public string PrintM()
+        {
+            string print = String.Empty;
+
+            for (int i = 0; i < nRows; i++) //step through each row in the matrix
+            {
+                for (int k = 0; k < nCols; k++) //print each column in the current row
+                {
+                    print += m[i, k] + " ";
+                }
+
+                print += "\n";
+            }
+
+            return print;
+        }//PrintM()
+
+        /// <summary>
+        /// overload the + operator to give ability to add Matrices
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <returns></returns>
+        public static Matrix operator+ (Matrix A, Matrix B)
+        {
+            Matrix C = new Matrix(A.nRows, A.nCols);
+            double sum;
+
+            for (int i = 0; i < A.nRows; i++)
+            {
+                for (int k = 0; k < A.nCols; k++)
+                {
+                    sum = A.m[i,k] + B.m[i,k];
+                    C.SetValue(i, k, sum);
+                }
+                                
+            }
+
+            return C;
+        }//operator+ (Matrix, Matrix)
+
+    }//class
+}//namespace
---
+++ Horne_Matrix_Lab/Horne_Matrix_Lab/Program.cs
@@ -0,0 +1,100 @@
+﻿/*************************************************************************
+ * File Name         : Program.cs
+ * Purpose           : Driver for matrix program. Asks the user to 
+ *                     enter sizes and values for 2 matrices. Adds the
+ *                     2 matrices and prints the resulting matrix.
+ *                          
+ * Author            : Austin Horne    E-mail: hornew@goldmail.etsu.edu       
+ * Course            : CSCI 3230 - Algorithms
+ * Create Date       : February 11, 2013
+ * 
+ * Modified Date     : February 11, 2013
+ * Modified by       : Austin Horne
+**************************************************************************
+*/
+using System;
+
+namespace Horne_Matrix_Lab
+{
+    class Program
+    {
+        static void Main()
+        {
+            Matrix A, B, C;
+            string input;
+            int Arow, Acol, Brow, Bcol;
+            double val;
+            
+            //ask for rows of matrix A. Parse value from keyboard
+            Console.WriteLine("\n\nEnter the number of rows for matrix A");
+            input = Console.ReadLine();
+            Int32.TryParse(input, out Arow);
+            
+            //ask for columns of matrix A. Parse value from keyboard
+            Console.WriteLine("Enter the number of columns for matrix A");
+            input = Console.ReadLine();
+            Int32.TryParse(input, out Acol);
+            
+            //create matrix A
+            A = new Matrix(Arow, Acol);
+
+            Console.WriteLine("Enter values for the matrix until the matrix is full\n");                                                        
+
+            //enter values sequentially into matrix.
+            for (int i = 0; i < Arow; i++)
+            {
+                for (int k = 0; k < Acol; k++)
+                {
+                    Console.Write("Value: ");
+                    input = Console.ReadLine();
+                    Double.TryParse(input, out val);
+
+                    A.SetValue(i, k, val);
+                }//for
+            }//for
+
+            //ask for rows of matrix B. Parse value from keyboard
+            Console.WriteLine("\n\nEnter the number of rows for matrix B");
+            input = Console.ReadLine();
+            Int32.TryParse(input, out Brow);
+
+            //ask for columns of matrix B. Parse value from keyboard
+            Console.WriteLine("Enter the number of columns for matrix B");
+            input = Console.ReadLine();
+            Int32.TryParse(input, out Bcol);
+
+            //create matrix B
+            B = new Matrix(Arow, Bcol);     
+
+            Console.WriteLine("Enter values for the matrix until the matrix is full\n");
+
+            //enter values sequentially into matrix.
+            for (int i = 0; i < Arow; i++)
+            {
+                for (int k = 0; k < Bcol; k++)
+                {
+                    Console.Write("Value: ");
+                    input = Console.ReadLine();
+                    Double.TryParse(input, out val);
+
+                    B.SetValue(i, k, val);
+                }//for
+            }//for
+
+            //print the result of adding the 2 matrices if the matrices are the same size. Notify if they are not
+            try
+            {
+                C = A + B;  //add matrices. Uses overloaded + operator in Matrix class
+                Console.WriteLine("\n\nResulting matrix\n" + C.PrintM());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n\nMatrices are not equal in size and cannot be added.");
+            }
+
+
+        }//main
+
+    }//class
+
+}//namespace
